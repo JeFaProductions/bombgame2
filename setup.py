12ae9491ebdc8af813dd6d5ecb8f8e4a830b205b
Fabian Meyer <user8324@posteo.net>
2019-01-31 22:57:39 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+#
+# Author: Fabian Meyer
+# Created On: 31 Jan 2019
+
+from distutils.core import setup
+
+setup(name='bombgame2',
+    version='0.1',
+    description='',
+    license='MIT',
+    author=['Fabian Meyer', 'Jens Gansloser'],
+    author_email='',
+    packages=['bombgame'],
+    ext_modules=[],
+    scripts=['bombgame2.py'])
